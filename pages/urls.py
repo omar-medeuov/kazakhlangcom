70a414692992905803312e17069dd1b90ec318d5
omar-medeuov <omarmedeuov@gmail.com>
2025-05-25 18:46:30 +0500

    simple article view added

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import article_view
+
 urlpatterns = [
+    path("", article_view(), name="article-view"),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
-# Create your views here.
+
+def article_view(request):
+    return TemplateResponse(request, "pages/article.html")
