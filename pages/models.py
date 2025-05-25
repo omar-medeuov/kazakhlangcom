38618ba65675339c624ede869285d5b3a88d5e45
omar-medeuov <omarmedeuov@gmail.com>
2025-05-25 21:41:44 +0500

    Article model added

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(help_text="Markdown format is to be used.")
+    date_published = models.DateTimeField(auto_now_add=True)
+
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 
+from .models import Article
 
-def article_view(request):
-    return TemplateResponse(request, "pages/article.html")
+def article_view(request, article_url):
+
+    article = Article.objects.get(title=article_url)
+
+    return TemplateResponse(request, "pages/article.html", context={
+        "article": article,
+    })
