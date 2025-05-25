012544d61abd4d79bc7ca659de3ae9c6f50c285f
omar-medeuov <omarmedeuov@gmail.com>
2025-05-25 18:49:26 +0500

    pages/urls.py: removed parentheses from function view

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import article_view
 
 urlpatterns = [
-    path("", article_view(), name="article-view"),
+    path("", article_view, name="article-view"),
 
 ]
