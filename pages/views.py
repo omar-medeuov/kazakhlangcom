029c21b429d68788f11c3c0efd051df08965599b
omar-medeuov <omarmedeuov@gmail.com>
2025-05-25 22:13:37 +0500

    markdown basic implementation

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 
+import markdown
+
 from .models import Article
 
 def article_view(request, article_url):
 
     article = Article.objects.get(title=article_url)
 
+    markdown_content = markdown.markdown(article.content, extensions=['extra', 'codehilite'])
+
     return TemplateResponse(request, "pages/article.html", context={
         "article": article,
+        "markdown_content": markdown_content,
     })
