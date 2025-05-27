e7937be52aa223a0efab3f46f2c960bd882ddf16
omar-medeuov <omarmedeuov@gmail.com>
2025-05-27 11:03:54 +0500

    ALLOWED_HOSTS made into env

--- kazakhlangcom/settings.py
+++ kazakhlangcom/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "192.168.0.43"]
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
